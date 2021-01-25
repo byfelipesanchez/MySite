27421d4c7af156b370668bb36f73c42d6dc32caa
Felipe Sánchez <2018.felipe.sanchez@gmail.com>
2021-01-24 21:17:58 -0300

    ..

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//intro slider animation
+
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
 tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 }); //text animation
@@ -5,14 +7,14 @@ tl.to(".slider", {y:"-100%", duration:1.5, delay:0.5}) //slider page comes in
 tl.to(".intro", {y:"-100%", duration:1})
 tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 2 });
 tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
-
-//end of intro animation
  
 
-//img-me fade in animation
+//name animation
 
 gsap.fromTo('.name', {autoAlpha:0}, {autoAlpha:1 , rotation:360 , delay:4.5})
 
+
+//img-me fade in animation
 
 const img = gsap.utils.toArray('.img-me');
 
@@ -26,9 +28,12 @@ img.forEach((img, i) => {
   });
 });
 
-//end of img-me anim
+//hr-name animation
 
-//nav anim
+gsap.fromTo(".hr-name", {opacity:0}, {opacity:1, delay:5.3})
+
+//nav(tool-bar) animation
+
 gsap.registerPlugin(ScrollTrigger)
 gsap.registerPlugin(MotionPathPlugin)
 
